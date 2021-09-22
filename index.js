3d5b2b2f0a7db0d96151718b6b8c305ffca08d54
Brendan Humphries <brendan.b.humphries@gmail.com>
2021-09-22 11:44:38 -0400

    commit tree d2cb2d79691f866b66fb543fd3d32d1324c73dae

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function superbowlWin(record) {
+    let isTrue = record.find(item => item.result === 'W');
+    if (isTrue === undefined) {
+        return undefined;
+    } else {
+        return record.find(item => item.result === 'W').year;
+    }
+}
